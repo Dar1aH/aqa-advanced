4b48dbfa9613ea6efd1b33cb969ea2a6aa757810
Dar1aH <daxundaintravel@gmail.com>
2024-10-29 22:22:25 +0000

    Added a new file

---
+++ nodeBasics/index.js
@@ -0,0 +1,6 @@
+import chalk from 'chalk'
+const log = console.log
+
+log(chalk.green.bgBlackBright.bold('Everything is amazing!'))
+log(chalk.red.bgWhiteBright.italic('Life is good!'))
+log(chalk.blueBright.bgYellowBright.dim('Study JS!'))
