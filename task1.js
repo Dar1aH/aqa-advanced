bee1310dfdb774fc32d2223c3eb27237a8a1201d
Dar1aH <daxundaintravel@gmail.com>
2024-11-04 17:57:28 +0000

    HW 4.1. JavaScript primitive data types

---
+++ task1.js
@@ -0,0 +1,14 @@
+//Створіть програму, в якій ви оголосите змінні для кожного з примітивних типів даних: рядок, число, буль, null та undefined. 
+//Присвойте їм значення та виведіть кожну змінну на консоль. 
+
+const text = 'Javascript'
+const number = 8
+const isAvailable = false
+const a = null
+const b = undefined
+
+console.log(text)
+console.log(number)
+console.log(isAvailable)
+console.log(a)
+console.log(b)
