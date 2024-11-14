afe550e726249e6eadf4751a0ee7c073861372b4
Dar1aH <daxundaintravel@gmail.com>
2024-11-14 01:18:45 +0000

    extra tasks

---
+++ HW6ExtraTasks/hw6_task2.js
@@ -0,0 +1,14 @@
+// Завдання 2
+// Створіть функцію яка приймає один параметр: age.
+// Усередині функції перевірте, чи age більше або рівне 18.
+// Якщо age відповідає умові, поверніть true, інакше поверніть false.
+// Викличте вашу функцію з аргументами 25 і 15, і виведіть результат (чи є особа дорослою) в консоль для кожного випадку.
+
+let person1 = 25
+let person2 = 15
+
+function isPersonAdult(age){
+return age>=18
+}
+console.log(`Is ${person1} an adult?`, isSubjectAdult(person1))
+console.log(`Is ${person2} an adult?`, isSubjectAdult(person2))
--- nodeBasics/practice3.js
+++
@@ -1,5 +0,0 @@
-                const x = 0.1
-                const y = 0.2
-                const sum = x + y 
-
-                console.log(sum.toFixed(2))
